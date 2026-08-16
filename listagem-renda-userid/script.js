3962755c01e75c028615d4b152955b1ed98299b7
Lucas <124069740+Renzolucas@users.noreply.github.com>
2026-08-15 22:26:17 -0300

    refactor: adicionando cores as frases de sucesso e erro de operacao

--- listagem-renda-userid/script.js
+++ listagem-renda-userid/script.js
@@ -1,17 +1,21 @@
 import { get, renderizarLista } from "../api.js";
 
-const divLista = document.getElementById('listaRendas')
-const btnRenda = document.getElementById('btnBuscar')
+const divLista = document.getElementById('listaRendas');
+const btnRenda = document.getElementById('btnBuscar');
 
 btnRenda.addEventListener('click', async function () {
     const userId = document.getElementById('userId').value;
     try {
-        const rendas = await get(`http://localhost:8080/users/${userId}/rendas`, "nao foi possivel fazer a listagem")
+        const rendas = await get( 
+            `http://localhost:8080/users/${userId}/rendas`,
+            "nao foi possivel fazer a listagem");
         renderizarLista(divLista, rendas, function (elemento, renda){
             elemento.textContent = `R$${renda.valor} - ${renda.dataHora}`;
+            elemento.style.color = '#4caf50';
         });
     } catch (erro) {
         divLista.textContent = 'Erro ao buscar rendas.'
+        divLista.style.color = '#e57373';
         console.error(erro);
     }
 });
